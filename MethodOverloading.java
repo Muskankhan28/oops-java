750ff4681c38d616309e36b55783e9d16baf9deb
Muskankhan28 <Muskankhan.khan5315@gmail.com>
2024-11-13 16:06:43 +0530

    example of method overloading in java

---
+++ MethodOverloading.java
@@ -0,0 +1,19 @@
+class Sum{
+    public int sum(int x , int y){
+        return x+y;
+    }
+    public int sum(int x , int y , int z){
+        return  x+y+z;
+    }
+    public double sum(double x , double y){
+        return x+y;
+    }
+}
+public class MethodOverloading {
+    public static void main(String[] args) {
+        Sum s  = new Sum();
+        System.out.println(s.sum(10, 20));
+        System.out.println(s.sum(10.5, 5.0));
+        System.out.println(s.sum(2, 2, 3)); 
+    }
+}
